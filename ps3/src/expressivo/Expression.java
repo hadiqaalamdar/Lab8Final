b2947dfc0ab71cea8ca33cf839afb9254a41c95f
hadiqaalamdar <47317024+hadiqaalamdar@users.noreply.github.com>
2019-04-01 16:16:40 +0500

    update1 by hadiqa
    expression.java

--- ps3/src/expressivo/Expression.java
+++ ps3/src/expressivo/Expression.java
@@ -2,6 +2,10 @@
  * Redistribution of original or derived work requires permission of course staff.
  */
 package expressivo;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.misc.ParseCancellationException;
+import org.antlr.v4.runtime.tree.*;
+import expressivo.parser.*;
 
 /**
  * An immutable data type representing a polynomial expression of:
@@ -16,8 +20,12 @@ package expressivo;
  */
 public interface Expression {
     
-    // Datatype definition
-    //   TODO
+    // Datatype definition:
+	// 
+	// Expression ::= 
+	// 		Primitive(p: Primitive) + BinOp(left: Primitive, right: Primitive)
+	// Primitive ::= 
+	//		Number(n: int) + Number(n: float) + Variable(v: String)
     
     /**
      * Parse an expression.
@@ -26,8 +34,59 @@ public interface Expression {
      * @throws IllegalArgumentException if the expression is invalid
      */
     public static Expression parse(String input) {
-        throw new RuntimeException("unimplemented");
+        CharStream stream = new ANTLRInputStream(input);
+        ExpressionLexer lexer = new ExpressionLexer(stream);
+        TokenStream tokens = new CommonTokenStream(lexer);
+        ExpressionParser parser = new ExpressionParser(tokens);
+        
+        parser.reportErrorsAsExceptions();
+        lexer.reportErrorsAsExceptions();
+
+        
+        ParseTree tree;
+        try {
+        	tree = parser.root();
+        } catch (ParseCancellationException ex) {
+        	String reason = ex.getMessage();
+        	String msg;
+        	if (reason != null)
+        		msg = "Syntax error in expression: " + reason;
+        	else
+        		msg = "Undefined syntax error in expression";
+
+    		throw new IllegalArgumentException(msg);
+        }
+        
+        ParseTreeWalker walker = new ParseTreeWalker();
+        ExpressionGenerator listener = new ExpressionGenerator();
+        
+        walker.walk(listener, tree);
+        
+        return listener.get();
     }
+    
+    /**
+     * Find whether this Expression will have precedence over another.
+     * If not, brackets will be needed to combine the two expressions in
+     * a human-readable format (eg a string).
+     * 
+     * @param other the second Expression 
+     * @return true if this Expression can be combined with other without
+     *         needing brackets, false otherwise
+     */
+    public boolean precedes(Expression other);
+    
+    /**
+     * @param variable Variable to differentiate by
+     * @return differential of this expression with respect to variable
+     */
+    public Expression differentiate(Variable variable);
+    
+    /**
+     * @return a version of this Expression in its simplest form, 
+     *         replacing e.g. Sum(Numeric, Numeric) with the sum of the values
+     */
+    public Expression simplify();
     
     /**
      * @return a parsable representation of this expression, such that
@@ -50,8 +109,5 @@ public interface Expression {
      *     e1.equals(e2) implies e1.hashCode() == e2.hashCode()
      */
     @Override
-    public int hashCode();
-    
-    // TODO more instance methods
-    
+    public int hashCode();   
 }
